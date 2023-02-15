658a10b9e0f2ca5b95011255a07e6247c09eb1bc
Manvanth <manvanthgs@gmail.com>
2023-02-15 21:45:02 +0530

    modified cpp file

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   cout << "My SRN is PES1UG20CS247\n";
+  cout << "My name is Manvanth Gs\n";
   return 0;
 }
